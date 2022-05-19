d586c8f8f4c391788566dc079fba8b381d6f02e1
Joshua Jones <joshua.desjones@gmail.com>
2022-05-19 08:40:30 -0500

    initialize firebase

---
+++ src/firebase/config.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBruRYocr8HLs90433fULNIRzBmqEgLlBo",
+  authDomain: "taskify-ea843.firebaseapp.com",
+  projectId: "taskify-ea843",
+  storageBucket: "taskify-ea843.appspot.com",
+  messagingSenderId: "45595105634",
+  appId: "1:45595105634:web:27e7b556cecf8c3946f43b",
+};
+
+const app = initializeApp(firebaseConfig);
