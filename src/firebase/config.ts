4b654616017face5ed87a1c8dfde8ef7504d9437
Joshua Jones <joshua.desjones@gmail.com>
2022-08-18 15:23:38 -0500

    Cleanup

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,7 @@
 import { initializeApp } from "firebase/app";
+import "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBruRYocr8HLs90433fULNIRzBmqEgLlBo",
@@ -9,4 +12,11 @@ const firebaseConfig = {
   appId: "1:45595105634:web:27e7b556cecf8c3946f43b",
 };
 
+//initialize firebase
 const app = initializeApp(firebaseConfig);
+
+//intialize services
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
